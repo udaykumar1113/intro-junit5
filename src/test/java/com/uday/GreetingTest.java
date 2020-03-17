1d2e15c2a47ea112549b9187b7e263cf9e73cb72
udayk <udaykumar1113@gmail.com>
2020-03-17 09:32:36 -0400

    @BeforeEach annotation usage

---
+++ src/test/java/com/uday/GreetingTest.java
@@ -0,0 +1,27 @@
+package com.uday;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GreetingTest {
+
+    private Greeting greeting;
+
+    @BeforeEach
+    void setUp(){
+        greeting=new Greeting();
+        System.out.println("Calling @BeforeEach annotated method");
+    }
+
+    @Test
+    void helloWorld() {
+        System.out.println(greeting.helloWorld());
+    }
+
+    @Test
+    void helloWorld1() {
+        System.out.println(greeting.helloWorld("Junit 5 Testing"));
+    }
+}
